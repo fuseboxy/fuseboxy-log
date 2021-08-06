f3066922a83692a8afdc38018b67674fb752adf3
henrygotmojo <henrygotmojo@gmail.com>
2021-08-06 11:25:56 +0800

    apply phpQuery to log-row (to avoid hard-code column width)

--- app/view/log/row.php
+++ app/view/log/row.php
@@ -2,55 +2,41 @@
 <fusedoc>
 	<io>
 		<in>
-			<object name="$bean">
-				<string name="action" />
-				<datetime name="datetime" />
-				<string name="username" />
-				<string name="sim_user" />
-				<string name="entity_type" />
-				<number name="entity_id" />
-				<string name="remark" />
-				<string name="ip" />
-			</object>
 			<string name="filterField" scope="$arguments" optional="yes" />
 			<string name="filterValue" scope="$arguments" optional="yes" />
 		</in>
 		<out />
 	</io>
 </fusedoc>
-*/ ?>
-<div id="log-row-<?php echo $bean->id; ?>" class="log-row scaffold-row small">
-	<table class="table table-hover table-sm mb-0">
-		<tbody>
-				<td width="60" class="col-id"><?php echo $bean->id; ?></td>
-				<td width="13%" class="col-datetime">
-					<div class="col-date"><?php echo date('Y-m-d', strtotime($bean->datetime)); ?></div>
-					<div class="col-time small text-muted"><?php echo date('H:i:s', strtotime($bean->datetime)); ?></div>
-				</td>
-				<td width="13%" class="col-username-sim_user">
-					<div class="col-username"><?php echo $bean->username; ?></div>
-					<div class="col-sim_user small text-muted"><?php echo $bean->sim_user; ?></div>
-				</td>
-				<td width="13%" class="col-action-ip">
-					<div class="col-action"><?php echo $bean->action; ?></div>
-					<div class="col-ip small text-muted"><?php echo $bean->ip; ?></div>
-				</td>
-				<td width="13%" class="col-entity_id-entity_type">
-					<div class="col-entity_id"><?php echo $bean->entity_id; ?></div>
-					<div class="col-entity_type small text-muted"><?php echo $bean->entity_type; ?></div>
-				</td>
-				<td width="30%" class="col-remark" style="word-break: break-all;"><?php
-					$str = $bean->remark;
-					if ( isset($arguments['filterField']) and $arguments['filterField'] == 'remark' and !empty($arguments['filterValue']) ) {
-						$startPos = stripos($str, $arguments['filterValue']);
-						$endPos = stripos($str, $arguments['filterValue']) + strlen($arguments['filterValue']);
-						$str = substr($str, 0, $endPos).'</mark>'.substr($str, $endPos);
-						$str = substr($str, 0, $startPos).'<mark>'.substr($str, $startPos);
-					}
-					echo nl2br($str);
-				?></td>
-				<td class="col-button text-right"><?php include F::appPath('view/scaffold/row.button.php'); ?></td>
-			</tr>
-		</tbody>
-	</table>
-</div>
+*/
+// capture original output
+ob_start();
+include F::appPath('view/scaffold/row.php');
+$output = ob_get_clean();
+
+
+// adjust output (when necessary)
+if ( class_exists('Util') ) :
+	$doc = Util::phpQuery($output);
+	// split date & time
+	$datetime = $doc->find('td.col-datetime div.col-datetime')->text();
+	ob_start();
+	?><div class="col-date"><?php echo date('Y-m-d', strtotime($datetime)); ?></div><?php
+	?><div class="col-time small text-muted"><?php echo date('H:i:s', strtotime($datetime)); ?></div><?php
+	$doc->find('td.col-datetime div.col-datetime')->after( ob_get_clean() )->remove();
+	// highlight remark
+	if ( isset($arguments['filterField']) and $arguments['filterField'] == 'remark' and !empty($arguments['filterValue']) ) :
+		$remark = $doc->find('td.col-remark div.col-remark')->html();
+		$startPos = stripos($remark, $arguments['filterValue']);
+		$endPos = stripos($remark, $arguments['filterValue']) + strlen($arguments['filterValue']);
+		$remark = substr($remark, 0, $endPos).'</mark>'.substr($remark, $endPos);
+		$remark = substr($remark, 0, $startPos).'<mark>'.substr($remark, $startPos);
+		$doc->find('td.col-remark div.col-remark')->html($remark);
+	endif;
+	// put into result
+	$output = $doc;
+endif;
+
+
+// display
+echo $output;
