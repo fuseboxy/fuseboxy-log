86d285c460f8fb436aa1ed8a348526f03baffaa8
henrygotmojo <henrygotmojo@gmail.com>
2022-04-27 11:08:08 +0800

    show remark with badge heading

--- app/view/log/row.php
+++ app/view/log/row.php
@@ -4,6 +4,17 @@
 		<in>
 			<string name="filterField" scope="$arguments" optional="yes" />
 			<string name="filterValue" scope="$arguments" optional="yes" />
+			<object name="$bean" type="log">
+				<number name="id" />
+				<datetime name="datetime" />
+				<string name="user" />
+				<string name="sim_user" />
+				<string name="action" />
+				<string name="ip" />
+				<string name="entity_type" />
+				<string name="entity_id" />
+				<string name="remark" />
+			</object>
 		</in>
 		<out />
 	</io>
@@ -19,13 +30,27 @@ $output = ob_get_clean();
 if ( class_exists('Util') ) :
 	$doc = Util::phpQuery($output);
 	// split date & time
-	$datetime = $doc->find('td.col-datetime div.col-datetime')->text();
 	ob_start();
-	?><div class="col-date"><?php echo date('Y-m-d', strtotime($datetime)); ?></div><?php
-	?><div class="col-time small text-muted"><?php echo date('H:i:s', strtotime($datetime)); ?></div><?php
-	$doc->find('td.col-datetime div.col-datetime')->after( ob_get_clean() )->remove();
+	?><div class="col-date"><?php echo date('Y-m-d', strtotime($bean->datetime)); ?></div><?php
+	?><div class="col-time small text-muted"><?php echo date('H:i:s', strtotime($bean->datetime)); ?></div><?php
+	$doc->find('td.col-datetime div.col-datetime')->html(ob_get_clean());
 	// word-break remark
-	$remark = $doc->find('td.col-remark div.col-remark')->attr('style', 'word-break: break-all;');
+	$doc->find('td.col-remark div.col-remark')->attr('style', 'word-break: break-all;');
+	// parse remark (when necessary)
+	$remarkRows = explode("\n", $bean->remark);
+	foreach ( $remarkRows as $i => $row ) {
+		// check if row matches [XXXXX] XXXXXXXXXXX} format
+		if ( strpos($row, '[') === 0 and strpos($row, '] ') !== false ) {
+			$row = preg_replace(['/\[/', '/]/'], ['<span class="badge badge-light border mr-1">','</span>'], $row, 1);
+		}
+		// check if having before & after values
+		if ( strpos($bean->action, 'UPDATE_') === 0 and strpos($row, ' ===> ') !== false ) {
+			$row = preg_replace('/ ===> /', '<span class="text-primary mx-1"> ===> </span>', $row, 1);
+		}
+		// append modified row
+		$remarkRows[$i] = $row;
+	}
+	$doc->find('td.col-remark div.col-remark')->html(implode('<br />', $remarkRows));
 	// highlight remark
 	if ( isset($arguments['filterField']) and $arguments['filterField'] == 'remark' and !empty($arguments['filterValue']) ) :
 		$remark = $doc->find('td.col-remark div.col-remark')->html();
